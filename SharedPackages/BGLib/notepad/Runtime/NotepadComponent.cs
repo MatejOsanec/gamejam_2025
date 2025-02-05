ccecdd6b044bfa5bb2a2c5a389915c38c1a1eb36
Matej Osanec <matejosanec@fb.com>
2025-02-05 13:11:18 +0100

    rm filesystem helper and uglyfix notepad

--- SharedPackages/BGLib/notepad/Runtime/NotepadComponent.cs
+++ SharedPackages/BGLib/notepad/Runtime/NotepadComponent.cs
@@ -1,9 +1,7 @@
-using BGLib.HierarchyIcons;
 using UnityEngine;
 
 namespace Notepad {
 
-    [HierarchyIcon("Contains a note", Icon.Note, "#828282", "Some child contains a note", Icon.NoteParent, "#5f5f5f")]
     public class NotepadComponent : MonoBehaviour {
 
         public NoteSO note;
