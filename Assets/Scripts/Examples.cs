13ff2cb16e9b2494158cd214e8ab9324aaea04ec
Matej Osanec <matejosanec@fb.com>
2025-02-06 19:25:18 +0100

    Add examples

---
+++ Assets/Scripts/Examples.cs
@@ -0,0 +1,44 @@
+using Beatmap;
+using Beatmap.Lightshow;
+using Core;
+
+public class Examples
+{
+    public Examples()
+    {
+        Locator.Callbacks.AddBeatListener(BeatDivision.Quarter, BeatHandler); // triggers BeatHandler every Quarter note, passes beatcount as int param
+        
+        Locator.Callbacks.NoteSpawnedSignal.AddListener(NoteSpawnedHandler); // triggers note spawned (currently 4 beats ahead its time in beatmap)
+        Locator.Callbacks.NoteMissSignal.AddListener(NoteMissHandler); // triggers note missed (exactly on time in beatmap)
+        
+        Locator.Callbacks.EventTriggeredSignal.AddListener(EventTriggeredHandler); // triggers when any event is passes, passes whole BeatmapEventData as param
+        Locator.Callbacks.AddEventListener(19, SpecificEventHandler); // triggers when event with specific id passes, passes the 0-1 float value of event directly
+    }
+
+    private void SpecificEventHandler(float v)
+    {
+        
+    }
+
+    private void EventTriggeredHandler(BeatmapEventData e)
+    {
+        
+    }
+
+    private void NoteMissHandler(ColorNote n)
+    {
+        
+    }
+
+    private void NoteSpawnedHandler(ColorNote n)
+    {
+        
+    }
+
+    private void BeatHandler(int b)
+    {
+        
+    }
+}
+
+    
