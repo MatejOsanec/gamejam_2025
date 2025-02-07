dde22a5229a151cd2ca1c0d2f28cfbd4ed12e6bf
metarob <robbogucki@fb.com>
2025-02-07 15:43:05 +0100

    add baddie controller

---
+++ Assets/Scripts/Gameplay/BaddieController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaddieController : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+
+
+    // Update is called once per frame
+    void SetMaterial(Material material)
+    {
+        if (_renderer != null)
+        {
+            _renderer.material = material;
+        }
+    }
+}
