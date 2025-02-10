a4271ebd12d12cb29de715fa9baf7d94edc4bdd7
Matej Osanec <matejosanec@fb.com>
2025-02-10 15:55:42 +0100

    tidying stuff

--- Assets/Scripts/Gameplay/PerlinNoiseModDefSo.cs
+++ Assets/Scripts/Gameplay/PerlinNoiseModDefSo.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using Core;
 using UnityEngine;
 
@@ -8,22 +6,16 @@ namespace Gameplay
     [CreateAssetMenu(fileName = "PerlinNoiseModDefSo", menuName = "ScriptableObjects/PerlinNoiseModDefSo", order = 1)]
     public class PerlinNoiseModDefSo : ModulationDefSo
     {
-        // just hiding original params
-        public new AnimationCurve movementCurve { get; private set; }
-        public new float syncMultiplier { get; private set; }
-        public new float offset { get; private set; }
-        // -----
-
         [Range(0, 1)] public float paramY = 1;
         
         public override float GetProgress()
         {
-            return Mathf.PerlinNoise(Locator.BeatModel.GetBeatProgress(8f), paramY);
+            return Mathf.PerlinNoise(Locator.BeatModel.GetBeatProgress(SyncMultiplier, Offset), paramY) * 2 - 1;
         }
 
         public override float GetProgressWithCustomOffset(float overrideOffset)
         {
-            return Mathf.PerlinNoise(Locator.BeatModel.GetShapedBeatProgress(Waveform.Sinusoidal, 8f, overrideOffset), paramY) * 2 - 1;
+            return Mathf.PerlinNoise(Locator.BeatModel.GetBeatProgress(SyncMultiplier, Offset), paramY) * 2 - 1;
         }
     }
 }
